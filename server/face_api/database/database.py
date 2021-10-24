e982276a6e7a107a42a0a1ff8e334346c4b37b75
Corban Villa <corbanvilla@gmail.com>
2021-10-24 16:56:16 +0400

    added sqlite fix

--- server/face_api/database/database.py
+++ server/face_api/database/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = f"{DATABASE_URL}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}  # Required for sqlite3
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
