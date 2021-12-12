72749fbcd87c5bb159a5754d70c02ac12880e28c
Corban Villa <corbanvilla@gmail.com>
2021-12-12 17:24:14 +0400

    updated dockerfiles

---
+++ serv_webrtc/docker/entrypoint-webrtc.sh
@@ -0,0 +1,8 @@
+python3 face_processor/main.py \
+ --verbose \
+ --signaling=node-dss \
+ --signaling-host=$LOCAL_PEER \
+ --signaling-port=$SIGNALING_PORT \
+ --local-peer=$LOCAL_PEER \
+ --remote-peer=$REMOTE_PEER
+
